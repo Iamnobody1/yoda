6c598aa40342f53d056d942c837f152a27dec619
Fluke <suphakit_1122@hotmail.co.th>
2022-06-02 17:58:31 +0700

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-white text-3xl font-bold underline">Hello world!</h1>
-  );
+  return <h1 className=" text-3xl font-bold underline">Fluke</h1>;
 }
 
 export default App;
