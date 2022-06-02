e3cab316515ee1d49438e0477d2f2f2be2f9c040
NatOb <nat@ookbee.com>
2022-06-02 17:57:13 +0700

    update app title

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-white text-3xl font-bold underline">Hello world!</h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
