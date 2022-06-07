3c28bfd3ff9aa8c0bd6c07921382659918078987
Fluke <suphakit_1122@hotmail.co.th>
2022-06-07 20:14:25 +0700

    husky commit

---
+++ next-ts/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Index() {
+  return (
+    <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-emerald-400 to-blue-500">
+      React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default Index;
