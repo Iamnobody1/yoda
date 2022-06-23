3b46ad5ff1a1b38336897f32a28745e5f7dc1f7f
Eikkew <Thammajade.kewq@mail.kmutt.ac.th>
2022-06-23 14:50:45 +0700

    Add customer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
 function Index() {
   return (
-    <h1 className='bg-gradient-to-r from-emerald-400 to-blue-500 bg-clip-text text-4xl font-extrabold text-transparent'>
-      React Typescript Tailwind Sample
-    </h1>
+    <>
+      <h1 className="bg-gradient-to-r from-emerald-400 to-blue-500 bg-clip-text text-4xl font-extrabold text-transparent">
+        React Typescript Tailwind Sample
+      </h1>
+    </>
   )
 }
 
